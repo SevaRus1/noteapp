c83b0ec37d422bd267ec4209b419ec830b2e2e44
SevaRus1 <9026867@gmail.com>
2026-05-13 20:48:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -308,5 +308,14 @@ def api_stats():
     })
 
 
+with app.app_context():
+    admin = User.query.filter_by(username='admin').first()
+    if not admin:
+        admin = User(username='admin', email='admin@example.com', password='admin123')
+        db.session.add(admin)
+        db.session.commit()
+        print('Тестовый аккаунт создан: admin / admin123')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
